211e2b709cce046e5c1cfe4e7169f62a8929ea04
azizanwazir <azizan.wazir.17@ucl.ac.uk>
2017-10-24 00:05:46 +0100

    Started on insertion sort code

--- insertionsort-c.c
+++ insertionsort-c.c
@@ -1,1 +1,34 @@
+/* Insertion sort as a function
+*  To retrieve the values of
+*
+*
+*/
 
+#include <stdio.h>
+
+long int * insertionsort(long int arr[])
+  {
+    long int n, i, j, previousValue;
+    n = arr.length;
+    static long int arrNum[];
+
+    for(i = 0; i < n; i++) //copy array
+      {
+        arrNum[i] = arr[i];
+      }
+
+    for(i = 1; i < n; i++) //outer loop
+      {
+        j = n
+        previousValue = arrNum[j - 1];
+
+        while(j > 0 && previousValue > arrNum[j]) //inner loop
+          {
+            arrNum[j - 1] = arrNum[j];
+            arrNum[j] = previousValue;
+          };
+        j--;
+      };
+
+      return arrNum;
+  }
